692381ee27ce06365112da864b89be383e5582b5
User <User@416-4>
2022-04-27 14:43:41 +0300

    ty

--- LaEnterpriseInternational/WelcomePeople.aspx.cs
+++ LaEnterpriseInternational/WelcomePeople.aspx.cs
@@ -4,11 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.OleDb;
+
+
 
 namespace LaEnterpriseInternational
 {
     public partial class WelcomePeople : System.Web.UI.Page
     {
+        static string connectionString = getConnectionString();
+        private OleDbConnection myConnection = new OleDbConnection(connectionString);
+        private OleDbTransaction objTrans;
+
+        const string FILE_NAME = "LaGroup.accdb";
+        public static string getConnectionString()
+        {
+            string location = HttpContext.Current.Server.MapPath("~/App_Data/" + FILE_NAME);
+            string ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0 ; data source=" + location;
+
+            return ConnectionString;
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
             
@@ -24,7 +40,36 @@ namespace LaEnterpriseInternational
 
         protected void buttonSubmit_Click1(object sender, EventArgs e)
         {
-            Response.Write("hi!" + email.Value.ToString());
+            Response.Write("hi " + name.Value.ToString()+"!");
+
+            OleDbCommand objCmd = new OleDbCommand("InsertMessage", myConnection);
+            objCmd.CommandType = CommandType.StoredProcedure;
+            OleDbParameter objParam;
+
+
+            objParam = objCmd.Parameters.Add("@Email", OleDbType.BSTR);
+            objParam.Direction = ParameterDirection.Input;
+            objParam.Value = email.Value;
+
+            objParam = objCmd.Parameters.Add("@Name", OleDbType.BSTR);
+            objParam.Direction = ParameterDirection.Input;
+            objParam.Value = name.Value;
+
+
+            try
+            {
+
+                this.myConnection.Open();
+                objCmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                this.myConnection.Close();
+            }
         }
 
         // Display the greeting label text.
